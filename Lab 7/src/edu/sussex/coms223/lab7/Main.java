da7122a1c724a04cb37466a4db815cebbd010e15
jsissler <jsissler@gmail.com>
2020-03-11 14:59:53 -0400

    Added merge

--- Lab 7/src/edu/sussex/coms223/lab7/Main.java
+++ Lab 7/src/edu/sussex/coms223/lab7/Main.java
@@ -3,6 +3,7 @@ package edu.sussex.coms223.lab7;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * The class Main benchmarks worst-case search performance of an OrderedList vs.
@@ -15,8 +16,13 @@ public class Main {
 	};
 
 	public static void main(String[] args) {
+		benchmark();
+		merge();
+	}
+
+	static void benchmark() {
 		final int ITERATIONS = 10000;
-		final int COUNT = 2000;
+		final int COUNT = 1000;
 		final int LAST_VALUE = COUNT - 1; // This is the worst-case value to search for.
 
 		OrderedList<Integer> olist = new OrderedList<>();
@@ -68,6 +74,55 @@ public class Main {
 			sum++;
 		}
 		return sum;
+	}
+
+	static void dump(OrderedList<Integer> l, String name) {
+		System.out.println("list " + name);
+		Integer i = l.first();
+		while (i != null) {
+			System.out.println("\t" + i);
+			i = l.next();
+		}
+	}
+
+	static void merge() {
+		final int COUNT = 3;
+
+		OrderedList<Integer> l1 = new OrderedList<Integer>();
+		OrderedList<Integer> l2 = new OrderedList<Integer>();
+
+		Random rng = new Random();
+
+		for (int i = 0; i < COUNT; i++) {
+			l1.insert(rng.nextInt(100));
+			l2.insert(rng.nextInt(100));
+		}
+
+		dump(l1, "l1");
+		dump(l2, "l2");
+
+		OrderedList<Integer> merged = new OrderedList<>();
+		int l1i = 0, l2i = 0;
+
+		while (l1i < l1.size() && l2i < l2.size()) {
+			int comparison = l1.get(l1i).compareTo(l2.get(l2i));
+			if (comparison == 0) {
+				merged.add(l1.get(l1i));
+				l1i++;
+				l2i++;
+			} else if (comparison < 0) // l1 < l2
+				merged.add(l1.get(l1i++));
+			else // l1 > l2
+				merged.add(l2.get(l2i++));
+		}
+
+		while (l1i < l1.size())
+			merged.add(l1.get(l1i++));
+
+		while (l2i < l2.size())
+			merged.add(l2.get(l2i++));
+
+		dump(merged, "merged");
 	}
 
 }
