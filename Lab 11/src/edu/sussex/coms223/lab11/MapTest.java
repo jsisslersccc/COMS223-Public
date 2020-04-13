139d012178275d73bec310d7195bbd18bf271276
jsissler <jsissler@gmail.com>
2020-04-13 16:40:36 -0400

    add perf test

--- Lab 11/src/edu/sussex/coms223/lab11/MapTest.java
+++ Lab 11/src/edu/sussex/coms223/lab11/MapTest.java
@@ -5,20 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class MapTest {
 
-	public static Stream<Map<String, Integer>> maps() {
-		return Stream.of(new HashMap<String, Integer>(16, 0.75f));
-	}
-
-	@ParameterizedTest
-	@MethodSource("maps")
-	void testPut(Map<String, Integer> m) {
+	@Test
+	void testPut() {
+		Map<String, Integer> m = new HashMap<String, Integer>(16, 0.75f);
 		assertEquals(0, m.size());
 		assertEquals(null, m.put("1", 1));
 		assertEquals(null, m.put("0", 0));
@@ -34,9 +27,9 @@ class MapTest {
 		});
 	}
 
-	@ParameterizedTest
-	@MethodSource("maps")
-	void testRemove(Map<String, Integer> m) {
+	@Test
+	void testRemove() {
+		Map<String, Integer> m = new HashMap<String, Integer>(16, 0.75f);
 		assertEquals(0, m.size());
 		Random rng = new Random();
 		Set<Integer> mapped = new HashSet<Integer>();
@@ -58,31 +51,52 @@ class MapTest {
 		assertEquals(0, m.size());
 	}
 
-	@ParameterizedTest
-	@MethodSource("maps")
-	void testPerformance(Map<String, Integer> m) {
-		final int ITERATIONS = 1000;
-		Random rng = new Random();
-		for (int i = 0; i < ITERATIONS; i++) {
-			m.clear();
-			assertEquals(0, m.size());
-			final int COUNT = 1000;
-			for (int j = 0; j < COUNT; j++) {
-				int value = rng.nextInt(COUNT);
-				Integer existing = m.put("" + value, value);
-				assertTrue(existing == null || existing == value);
+	@Test
+	void testPerformance() {
+		Object[] maps = { new HashMap<String, Integer>(16, 0.75f), new HashMap<String, Integer>(16, Float.MAX_VALUE) };
+		long[] timeInMS = new long[2];
+
+		for (int i = 0; i < 2; i++) {
+			long startTimeMS = System.currentTimeMillis();
+
+			@SuppressWarnings("unchecked")
+			Map<String, Integer> m = (Map<String, Integer>) maps[i];
+
+			final int ITERATIONS = 1000;
+			Random rng = new Random();
+
+			for (int j = 0; j < ITERATIONS; j++) {
+				m.clear();
+				assertEquals(0, m.size());
+
+				final int COUNT = 1000;
+
+				for (int k = 0; k < COUNT; k++) {
+					int value = rng.nextInt(COUNT);
+					Integer existing = m.put("" + value, value);
+					assertTrue(existing == null || existing == value);
+				}
+
+				for (int k = 0; k < COUNT; k++) {
+					int value = rng.nextInt(COUNT);
+					Integer existing = m.get("" + value);
+					assertTrue(existing == null || existing == value);
+				}
+
+				for (int k = 0; k < COUNT; k++) {
+					int value = rng.nextInt(COUNT);
+					Integer existing = m.remove("" + value);
+					assertTrue(existing == null || existing == value);
+				}
 			}
-			for (int j = 0; j < COUNT; j++) {
-				int value = rng.nextInt(COUNT);
-				Integer existing = m.get("" + value);
-				assertTrue(existing == null || existing == value);
-			}
-			for (int j = 0; j < COUNT; j++) {
-				int value = rng.nextInt(COUNT);
-				Integer existing = m.remove("" + value);
-				assertTrue(existing == null || existing == value);
-			}
+
+			timeInMS[i] = System.currentTimeMillis() - startTimeMS;
+			System.out.println("timeInMS=" + timeInMS[i]);
 		}
+
+		// Uncomment out to test performance difference after implementing dynamic
+		// resizing.
+		// assertTrue(timeInMS[0] * 10 < timeInMS[1]);
 	}
 
 }
